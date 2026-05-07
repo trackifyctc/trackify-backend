f52544f0b1a82897dd2b496d6a9c5a54d497d6a9
trackifyctc <trackifyctc123@gmail.com>
2026-05-07 11:15:12 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       'http://localhost:3000',
+      'https://trackifyte.lid',
     ],
     credentials: true,
   });
